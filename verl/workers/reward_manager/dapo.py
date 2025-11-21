6cb0166abc1bb100b2f886f190e370bec996f1c6
Zhangmj <2270170683@qq.com>
2025-11-21 11:11:03 +0800

    add new verl update to fix reward abnormal in sglang fully async

--- verl/workers/reward_manager/dapo.py
+++ verl/workers/reward_manager/dapo.py
@@ -94,11 +94,7 @@ class DAPORewardManager(AbstractRewardManager):
 
             extra_info = data_item.non_tensor_batch.get("extra_info", {})
 
-            num_turns = data_item.non_tensor_batch.get("__num_turns__", None)
-
             rollout_reward_scores = data_item.non_tensor_batch.get("reward_scores", {})
-
-            extra_info["num_turns"] = num_turns
 
             extra_info["rollout_reward_scores"] = rollout_reward_scores
 
